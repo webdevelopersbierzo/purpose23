6d4263d0553f56c902ea194576a83385cbe5c07c
webdevelopersbierzo <1216455+webdevelopersbierzo@users.noreply.github.com>
2023-01-14 14:12:37 +0100

    commit init config backend

---
+++ backend/purpose23/src/main/java/com/webdeveloperbierzo/purpose23/Purpose23Application.java
@@ -0,0 +1,13 @@
+package com.webdeveloperbierzo.purpose23;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Purpose23Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Purpose23Application.class, args);
+	}
+
+}
---
+++ backend/purpose23/src/test/java/com/webdeveloperbierzo/purpose23/Purpose23ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.webdeveloperbierzo.purpose23;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Purpose23ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
