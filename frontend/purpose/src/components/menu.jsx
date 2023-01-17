1705037f562c8bb4b63c30515a87543e7d916472
webdevelopersbierzo <1216455+webdevelopersbierzo@users.noreply.github.com>
2023-01-17 14:06:48 +0100

    add heroicons, config tailwind and menu component

---
+++ frontend/purpose/src/components/menu.jsx
@@ -0,0 +1,30 @@
+import React from 'react';
+import { Bars3Icon, BellIcon } from '@heroicons/react/24/outline'
+
+const Menu = () => {
+    return (
+        <div className='flex justify-start items-center h-16 bg-gradiente-to-r from bg-fuchsia-500 to-violet-400'>
+            <div className='flex w-full justify-between m-4'>
+                    <span className='sr-only'>Open main menu</span>
+                    
+                    <Bars3Icon className='w-10 h-10 text-white'></Bars3Icon>
+                
+                    <p className='text-2xl text-white'>Purpose</p>
+                    <div className='flex justify-start items-center'>
+                        <BellIcon className='w-8 h-8 text-white'></BellIcon>
+                        <div className='flex justify-center rounded-full w-10 h-10 bg-white'>
+                            <p className='text-2xl text-gray-200 mt-1'>U</p> 
+                        </div>
+                    </div>
+                 
+                
+
+            </div>
+
+        </div>
+        
+    )
+};
+
+export default Menu;
+
