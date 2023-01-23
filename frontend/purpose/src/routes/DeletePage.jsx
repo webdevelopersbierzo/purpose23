2a2ae81c5d99f315c2fb8679ca2a62e552504662
webdevelopersbierzo <1216455+webdevelopersbierzo@users.noreply.github.com>
2023-01-23 10:08:12 +0100

    feat: add DeletePage

---
+++ frontend/purpose/src/routes/DeletePage.jsx
@@ -0,0 +1,63 @@
+import React, { useState, useEffect } from 'react';
+import PropTypes from 'prop-types';
+import Menu from '../components/menu';
+import axios from 'axios';
+import { useParams } from 'react-router';
+
+const DeletePage = () => {
+
+    const {id} = useParams;
+
+    const [purpose, setPurpose] = useState([]);
+    const [isReady, setIsReady] = useState(false)
+
+    useEffect(() => {
+      axios.get(`https://purpose23-production.up.railway.app/api/v1/purposes/6`)
+        .then((response)=> {
+            if(response.status === 200){
+                setPurpose(response.data)
+                setIsReady(true)
+            }
+        })
+        .catch(error => console.log(error));
+    }, [])
+    
+
+    const deletePurpose = () => {
+        axios.delete(`https://purpose23-production.up.railway.app/api/v1/purposes/6`)
+        .then(()=>{
+            alert("Post deleted");
+             setPurpose(null)
+        })
+    }
+    if(!purpose) return (
+        <div className="w-full bg-slate-900 h-screen">
+        <Menu />
+        <p>Purpose delete !!!!!</p>
+       
+        
+    </div>
+
+    )
+    return (
+        <div className="w-full bg-slate-900 h-screen">
+            <Menu />
+            <div className='flex flex-col w-full items-center  bg-gray-100'>
+                <h2 className='text-2xl p-4'>Delete purpose</h2>
+                <p> Danger you will erase a purpose</p>
+                <button type='submit' className='bg-red-500 pt-2 pb-2 pl-4 pr-4 mt-4 text-white' onClick={deletePurpose} disabled>Delete</button> 
+                           
+            </div>
+           
+            
+        </div>
+    );
+};
+
+
+DeletePage.propTypes = {
+
+};
+
+
+export default DeletePage;
