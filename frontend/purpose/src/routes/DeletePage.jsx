67462922429db45f2b460a544cc2a0946647fef9
webdevelopersbierzo <1216455+webdevelopersbierzo@users.noreply.github.com>
2023-01-26 19:51:08 +0100

    fix button, url param, redirect

--- frontend/purpose/src/routes/DeletePage.jsx
+++ frontend/purpose/src/routes/DeletePage.jsx
@@ -2,17 +2,19 @@ import React, { useState, useEffect } from 'react';
 import PropTypes from 'prop-types';
 import Menu from '../components/menu';
 import axios from 'axios';
-import { useParams } from 'react-router';
+import { useParams, useNavigate} from 'react-router';
 
 const DeletePage = () => {
 
-    const {id} = useParams;
+    const param = useParams();
+    const navigate = useNavigate()
 
+    console.log(param.id)
     const [purpose, setPurpose] = useState([]);
     const [isReady, setIsReady] = useState(false)
 
-    useEffect(() => {
-      axios.get(`https://purpose23-production.up.railway.app/api/v1/purposes/6`)
+     useEffect(() => {
+      axios.get(`https://purpose23-production.up.railway.app/api/v1/purposes/${param.id}`)
         .then((response)=> {
             if(response.status === 200){
                 setPurpose(response.data)
@@ -20,15 +22,21 @@ const DeletePage = () => {
             }
         })
         .catch(error => console.log(error));
-    }, [])
+    }, []) 
     
 
     const deletePurpose = () => {
-        axios.delete(`https://purpose23-production.up.railway.app/api/v1/purposes/6`)
+        axios
+        .delete(`https://purpose23-production.up.railway.app/api/v1/purposes/${param.id}`)
         .then(()=>{
             alert("Post deleted");
-             setPurpose(null)
+            setPurpose(null)
+            navigate("/profile")
         })
+        .catch(function(error) {
+            alert("Error-> Not delete")
+        })
+
     }
     if(!purpose) return (
         <div className="w-full bg-slate-900 h-screen">
@@ -45,7 +53,7 @@ const DeletePage = () => {
             <div className='flex flex-col w-full items-center  bg-gray-100'>
                 <h2 className='text-2xl p-4'>Delete purpose</h2>
                 <p> Danger you will erase a purpose</p>
-                <button type='submit' className='bg-red-500 pt-2 pb-2 pl-4 pr-4 mt-4 text-white' onClick={deletePurpose} disabled>Delete</button> 
+                <button type='submit' className='bg-red-500 pt-2 pb-2 pl-4 pr-4 mt-4 text-white' onClick={deletePurpose} >Delete</button> 
                            
             </div>
            
