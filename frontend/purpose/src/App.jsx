0c4eee330e4873be9db525a9ee691cfa9bc7d3be
webdevelopersbierzo <1216455+webdevelopersbierzo@users.noreply.github.com>
2023-01-30 09:13:53 +0100

    fix: mensajes menu

--- frontend/purpose/src/App.jsx
+++ frontend/purpose/src/App.jsx
@@ -3,6 +3,7 @@ import reactLogo from './assets/react.svg'
 import './App.css'
 import Menu from './components/menu'
 import PurposeList from './components/PurposeList'
+import { Link } from 'react-router-dom'
 
 function App() {
   const [count, setCount] = useState(0)
@@ -10,6 +11,10 @@ function App() {
   return (
     <div className='w-full bg-slate-900  h-screen text-white'>
       <Menu></Menu>
+      <div className='flex flex-col  text-2xl '>
+      <h2 className="text-3xl"> Mis propositos para esta 2023</h2>
+      <Link to={'/profile'}> HAZ CLICK AQUI</Link>
+      </div>
       
     </div>
   )
